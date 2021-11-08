54066c3ae92c8434187d3cdf35c178086029c62c
Avi Vadali <akvadali@gmail.com>
2021-11-08 17:04:25 -0600

    made changes to AviAuto1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AviAuto1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/AviAuto1.java
@@ -1,8 +1,6 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import org.firstinspires.ftc.teamcode.JacksonDriveTrain;
 
@@ -13,12 +11,28 @@ public class AviAuto1 extends LinearOpMode{
     public void runOpMode() {
         waitForStart();
         dt = new JacksonDriveTrain(this);
+        dt.driveAtHeading(0, 0, 1.5, 0.4);
+        // spinning
         dt.spinCarousel();
-        dt.driveAtHeading(125, 0, 31.8, 0.7);
-        dt.driveAtHeading(125, 27.4, 0, 0.7);
+        // scan barcode
+        dt.driveAtHeading(127, 0, 30, 0.7);
+        dt.driveAtHeading(127, 22, 0, 0.7);
         // color sensor range of 5cm
+        int i = 2;
         while(!dt.isTS() && opModeIsActive()) {
-             dt.driveAtHeading(125, 0, 24, 0.6);
+             dt.driveAtHeading(127, 0, 23.2, 0.6);
+             i--;
         }
+        // getting to shipping hub
+        int ts_pos = i;
+        for(int j = 0; j < i; j++) {
+            dt.driveAtHeading(127, 0, 23.2, 0.6);
+        }
+        dt.driveAtHeading(127, 0, 32, 0.6);
+        dt.driveAtHeading(127, 5, 0, 0.7);
+        dt.driveAtHeading(127, -5, 0, 0.7);
+        dt.driveAtHeading(37, 50, 0, 1);
+        dt.driveAtHeading(37, 0, -20, 0.6);
+        dt.driveAtHeading(37, 100, 0, 1);
     }
 }

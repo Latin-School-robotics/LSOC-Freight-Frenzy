5c72280a7968f263621350019035376cc6685b37
Gideon4 <gideon@heltzers.com>
2021-11-17 16:55:31 -0600

    Changed Jackson Drive Train
    Changed Jackson Drive Train to work with my robot and updated my autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ninety_Percent_Mine/Autonomous/NinetyPercentMine_Autonomous.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Ninety_Percent_Mine/Autonomous/NinetyPercentMine_Autonomous.java
@@ -3,15 +3,15 @@ package org.firstinspires.ftc.teamcode.Ninety_Percent_Mine.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.OldCode.JacksonDriveTrain;
+import org.firstinspires.ftc.teamcode.OldCode.GidThing;
 
 @Autonomous(name = "NinetyAuto", group = "Autos")
 public class NinetyPercentMine_Autonomous extends LinearOpMode {
-    JacksonDriveTrain dt;
+    GidThing dt;
     @Override
     public void runOpMode() {
         waitForStart();
-        dt = new JacksonDriveTrain(this);
+        dt = new GidThing(this);
         dt.driveAtHeading(0, 0, 1.5, 0.4);
         dt.driveAtHeading(127, 0, 30, 0.7);
         dt.driveAtHeading(127, 22, 0, 0.7);
